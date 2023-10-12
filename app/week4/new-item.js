627e3c73cec10174692991996b9ec8babec07964
kev257 <tuananh.nguyen@edu.sait.ca>
2023-10-11 20:09:18 -0600

    week4 not styled

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -1,4 +1,81 @@
 'use client'
 import { useState } from 'react'
 
+export function newItemFunction() 
+{
+    const [name, setName] = useState("");
+    const[quantity, setQuantity] = useState(1);
+    const[categories, setCategories] = useState(produce);
 
+    const handleSubmit = (e) => 
+    
+        e.preventDefault();
+        const newItem = 
+        {
+            name,
+            quantity,
+            category,
+        };
+    console.log(newItem);
+    alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
+
+    setName("");
+    setQuantity(0);
+    setCategory("");
+
+
+
+    return (
+        <div>
+          <h2>Create a New Item</h2>
+          <form onSubmit={handleSubmit}>
+           
+            <label>
+              Name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </label>
+    
+            
+            <label>
+              Quantity:
+              <input
+                type="number"
+                min="1"
+                max="99"
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+                required
+              />
+            </label>
+    
+            
+            <label>
+              Category:
+              <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="Produce">Produce</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Bakery">Bakery</option>
+                <option value="Meat">Meat</option>
+                <option value="Frozen Foods">Frozen Foods</option>
+                <option value="Canned Goods">Canned Goods</option>
+                <option value="Dry Goods">Dry Goods</option>
+                <option value="Beverages">Beverages</option>
+                <option value="Snacks">Snacks</option>
+                <option value="Household">Household</option>
+                <option value="Other">Other</option>
+              </select>
+            </label>
+    
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      );
+
+}
+
+export default NewItem;
